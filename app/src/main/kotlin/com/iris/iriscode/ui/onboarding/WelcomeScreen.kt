7dd0c5397326deb2976800ad54cecbc1083586a2
Muhofy <muhofy@iris.app>
2026-06-28 00:48:50 +0000

    fix(ui): LottieCompositionSpec is in compose package, not lottie

--- app/src/main/kotlin/com/iris/iriscode/ui/onboarding/WelcomeScreen.kt
+++ app/src/main/kotlin/com/iris/iriscode/ui/onboarding/WelcomeScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
